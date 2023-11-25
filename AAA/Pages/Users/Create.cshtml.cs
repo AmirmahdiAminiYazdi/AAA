b148876124dbe29000fd5f20edabb911e13a1491
amir mahdi amini yazdi <amirmahdi.amini@yahoo.com>
2023-11-26 01:51:39 +0330

    Implement manage password

--- AAA/Pages/Users/Create.cshtml.cs
+++ AAA/Pages/Users/Create.cshtml.cs
@@ -1,12 +1,52 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace AAA.Pages.Users
 {
     public class CreateModel : PageModel
     {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public CreateModel(UserManager<IdentityUser>userManager)
+        {
+            _userManager = userManager;
+        }
+        //[BindProperty]
+        //public string Name { get; set; }
+        //[BindProperty]
+        //public string Family { get; set; }
+        [BindProperty]
+        public string UserName { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+        [BindProperty]
+        [EmailAddress]
+        public string Email { get; set; }
         public void OnGet()
         {
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(ModelState.IsValid) 
+            {
+                IdentityUser user = new()
+                {
+                    UserName = UserName,
+                    Email = Email
+                };
+                IdentityResult result = await _userManager.CreateAsync(user,Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToPage("ListUser");
+                }
+                foreach(var item  in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+            return Page();
         }
     }
 }
--- AAA/Program.cs
+++ AAA/Program.cs
@@ -3,12 +3,32 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AAADbContext>(x=>x.UseNpgsql(connectionString));
-builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<AAADbContext>();
+builder.Services.AddIdentity<IdentityUser,IdentityRole>(x=>
+{
+    //example 1
+    x.Password.RequiredLength = 4;
+    x.Password.RequiredUniqueChars = 2;
+    x.Password.RequireNonAlphanumeric = false;
+    x.Password.RequireUppercase = false;
+    x.Password.RequireLowercase = false;
+    x.Password.RequireDigit = false;
+}).AddEntityFrameworkStores<AAADbContext>();
 builder.Services.AddRazorPages();
 builder.Services.AddMvc();
+
+//example 2
+//builder.Services.Configure<IdentityOptions>(c =>
+//{
+
+//});
+
 var app = builder.Build();
+
+
 app.UseDeveloperExceptionPage();
 app.UseStaticFiles();
 app.MapRazorPages();
