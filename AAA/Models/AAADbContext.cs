7c78e24cecb6aa162c040632cec258de2c6a8e6a
amir mahdi amini yazdi <amirmahdi.amini@yahoo.com>
2023-11-25 20:17:01 +0330

    initial project

---
+++ AAA/Models/AAADbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AAA.Models
+{
+    public class AAADbContext : IdentityDbContext<IdentityUser>
+    {
+        public AAADbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- AAA/Program.cs
+++ AAA/Program.cs
@@ -1,6 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using AAA.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AAADbContext>(x=>x.UseNpgsql(connectionString));
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<AAADbContext>();
+builder.Services.AddRazorPages();
+builder.Services.AddMvc();
+var app = builder.Build();
+app.UseDeveloperExceptionPage();
+app.UseStaticFiles();
+app.MapRazorPages();
+app.MapDefaultControllerRoute();
+
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
